f2eec797fae201886985cf6049471a55051e177c
Tole Mendoza <rodrigermoon@gmail.com>
2023-06-06 16:47:21 -0600

    Refactor the Bowling class and add comments for better code clarity and maintainability. The changes include:
    Added comments to describe the purpose and functionality of each method.
    Renamed variables for better readability.
    Replaced hardcoded numbers with constants.
    Reformatted code for improved indentation and spacing.
    Updated the JSDoc comments for the isStrike and isSpare methods.
    
    Co-authored-by: DerekAyala <derek.ayala12345@hotmail.com>

--- game.js
+++ game.js
@@ -1,11 +1,20 @@
+/**
+ * Bowling class, responsible for handling the logic of a bowling game.
+ */
 class Bowling {
+  /**
+   * Bowling constructor. Initializes throws and scores arrays.
+   */
   constructor() {
     this.throws = [];
     this.scores = [];
   }
-
+  /**
+   * Simulates a throw in a bowling game.
+   * Calculates the score and updates the throws and scores arrays.
+   */
   bowlingThrow() {
-    let throw1 = Math.floor(Math.random() * 11);
+    const throw1 = Math.floor(Math.random() * 11);
     let throw2 = 0;
     if (throw1 !== 10) {
       throw2 = Math.floor(Math.random() * (11 - throw1));
@@ -13,7 +22,7 @@ class Bowling {
 
     this.throws.push(throw1, throw2);
 
-    let round = this.throws.length / 2 - 1;
+    const round = this.throws.length / 2 - 1;
 
     let resultado = throw1 + throw2;
     if (round !== 0) {
@@ -28,15 +37,26 @@ class Bowling {
 
     this.scores.push(resultado);
   }
-
+  /**
+   * Checks if the throw in a specified round was a strike.
+   * @param {number} round - The round to check.
+   * @return {boolean} - True if it was a strike, false otherwise.
+   */
   isStrike(round) {
     return this.throws[round * 2] === 10;
   }
-
+  /**
+   * Checks if the throw in a specified round was a spare.
+   * @param {number} round - The round to check.
+   * @return {boolean} - True if it was a spare, false otherwise.
+   */
   isSpare(round) {
     return this.throws[round * 2] + this.throws[round * 2 + 1] === 10;
   }
 
+  /**
+   * Plays the complete bowling game.
+   */
   bowlingGame() {
     for (let round = 0; round < 10; round++) {
       this.bowlingThrow();
@@ -49,33 +69,38 @@ class Bowling {
     }
   }
 
+
+  /**
+   * Calculates and displays the bowling score.
+   */
   bowlingScore() {
-    let throws = "";
-    let score = "";
+    let throws = '';
+    let score = '';
     let frameScore = 0;
 
     for (let i = 0; i < 10; i++) {
       throws = throws.concat(
-        this.throws[i * 2],
-        String(this.throws[i * 2]).length === 2 ? " " : "  ",
+          this.throws[i * 2],
+        String(this.throws[i * 2]).length === 2 ? ' ' : '  ',
         this.throws[i * 2 + 1],
-        String(this.throws[i * 2 + 1]).length === 2 ? "" : " ",
-        "|"
+        String(this.throws[i * 2 + 1]).length === 2 ? '' : ' ',
+        '|',
       );
 
       frameScore = this.scores[i];
 
       score = score.concat(
-        frameScore,
-        String(frameScore).length === 1 ? "    " : String(frameScore).length === 2 ? "   " : "  ",
-        "|"
+          frameScore,
+        String(frameScore).length === 1 ? '    ' :
+          String(frameScore).length === 2 ? '   ' : '  ',
+        '|',
       );
     }
 
     if (this.throws.length > 20) {
       throws = throws.concat(
-        this.throws[20],
-        "  |"
+          this.throws[20],
+          '  |',
       );
     }
 
@@ -83,11 +108,16 @@ class Bowling {
     console.log(score);
 
     if (this.isStrike(9) || this.isSpare(9)) {
-      console.log(`¡Aquí hubo un ${this.isStrike(9) ? "strike" : "spare"} en el décimo frame! \n Los  ${this.throws[20]} puntos extra se han sumado a la puntuación final.\n`);
+      console.log(`¡Aquí hubo un 
+        ${this.isStrike(9) ? 'strike' : 'spare'} 
+        en el décimo frame! \n Los  
+        ${this.throws[20]} 
+        puntos extra se han sumado a la puntuación final.\n`,
+      );
     }
   }
 }
 
-const game = new Bowling();
-game.bowlingGame();
-game.bowlingScore();
+
+module.exports = Bowling;
+
