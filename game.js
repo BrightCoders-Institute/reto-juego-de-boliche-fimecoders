393baf5c3c3ae1a9dcd4817d960360150045940e
DerekAyala <derek.ayala12345@hotmail.com>
2023-06-05 19:11:24 -0600

    Implemented basic bowling game functionality for 10 rounds
    - Added the functionality for a basic game of bowling, consisting of 10 rounds with 2 throws each (without considering special rules like strikes or spares).
    - Implemented the necessary logic in the Bowling class to handle the scoring and progression of the game.
    - Managed the rounds and throws within the game, ensuring that the correct number of rounds is played and scores are recorded accurately.
    
    Co-authored-by: Tole15 <rodrigermoon@gmail.com>

--- game.js
+++ game.js
@@ -1,30 +1,60 @@
-const readline = require('readline')
-class Bowling{
-  constructor(){
+class Bowling {
+  constructor() {
     this.throw1 = [];
     this.throw2 = [];
     this.scores = [];
-    this.round = 1;
+    this.round = 0;
   }
 
-  bowlingThrow(){
-    const throw1= Math.floor(Math.random()*11);
-    this.throw1.push(throw1)
-    if (throw1 === 10){
-      this.throw2.push(0);
-    } else {
-      const throw2 = Math.floor(Math.random()*(11-throw1));
-      this.throw2.push(throw2);
+  bowlingThrow() {
+    const throw1 = Math.floor(Math.random() * 11);
+    let throw2 = 0;
+    this.throw1.push(throw1);
+    if (throw1 !== 10) {
+      throw2 = Math.floor(Math.random() * (11 - throw1));
     }
+
+    this.throw2.push(throw2);
+    let resultado = throw1 + throw2;
+    if (this.round !== 0) {
+      resultado += this.scores[this.round - 1];
+    }
+    this.scores.push(resultado);
   }
 
-  bowlingGame(){
-    while (this.round < 10){
+  bowlingGame() {
+    while (this.round < 10) {
       this.bowlingThrow();
       this.round++;
     }
   }
-}
-const game= new Bowling();
-const { throw1,throw2 }= game.bowlingthrow();
 
+  bowlingScore() {
+    let throws = "";
+    let score = "";
+    for (let i = 0; i < 10; i++) {
+      throws = throws.concat(
+        this.throw1[i],
+        String(this.throw1[i]).length === 2 ? " " : "  ",
+        this.throw2[i],
+        String(this.throw2[i]).length === 2 ? "" : " ",
+        "|"
+      );
+      score = score.concat(
+        this.scores[i],
+        String(this.scores[i]).length === 1
+          ? "    "
+          : String(this.scores[i]).length === 2
+          ? "   "
+          : "  ",
+        "|"
+      );
+    }
+    console.log(throws);
+    console.log(score);
+  }
+}
+
+const game = new Bowling();
+game.bowlingGame();
+game.bowlingScore();
